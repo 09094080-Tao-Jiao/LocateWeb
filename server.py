6d96657d5c3d7d68e3c91adf0945357ac24867eb
tao <Tao.Jiao@quantacn.com>
2017-09-21 21:28:21 +0800

    change default port

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ from tornado.options import define, options
 from application import application
 import tornado.autoreload
 
-define("port", default=8000, help="run on the given port", type=int)
+define("port", default=9000, help="run on the given port", type=int)
 if __name__ == "__main__":
     #tornado.options.parse_command_line()
     application.listen(options.port)
